c33a298f0a7a3f5dd371d6f992bf2efa13043255
dahee17 <133829893+dahee17@users.noreply.github.com>
2023-09-08 12:51:37 +0900

    Add python_01_variable

---
+++ lecture/python_01_variable.py
@@ -0,0 +1,10 @@
+#주석 : 단순 메모, 개발 실형 X
+
+#출력함수(print)
+#- ()안의 값을 출력
+#- 변수 값 확인 용도 또는 메세지 출력 용도
+print("hello python")
+
+#문자열 타입(string Type)
+#- Python: '', "" -> String
+#- C,Java: ''(Char),  ""(String)
